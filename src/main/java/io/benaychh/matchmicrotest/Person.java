056888c819a28fa95ccdf7676588c2763d2602d9
BenAychh <ben.tomas.hernandez@gmail.com>
2016-05-19 13:40:37 -0600

    basic tests

---
+++ src/main/java/io/benaychh/matchmicrotest/Person.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.benaychh.matchmicrotest;
+
+/**
+ *
+ * @author benhernandez
+ */
+public class Person {
+  private String name;
+  public Person(final String pName) {
+    this.name = pName;
+  }
+  public String getName() {
+    return this.name;
+  }
+}
